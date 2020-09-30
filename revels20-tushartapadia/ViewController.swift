79a56ab4d4ed254a75f3e6bf4c9bb8dd6d0e3bcd
rohitkuber2000 <rohitkuber42@gmail.com>
2020-10-01 00:14:29 +0530

    Added function to print odd numbers

--- revels20-tushartapadia/ViewController.swift
+++ revels20-tushartapadia/ViewController.swift
@@ -31,6 +31,17 @@ func firsteven()
         }
     }
 }
+    
+    func firstodd(){
+        
+        for i in 1...40{
+            if(i%2==0){
+                continue
+            }else{
+              print(i)
+            }
+        }
+    }
  
     
 func fibonacci()
